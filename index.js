19402ffbe19b2fa32eded5f26ffb0e778dd3cf82
Adan Shah <as3939984@gmail.com>
2024-05-08 06:20:10 +0500

    my first project push on github

---
+++ index.js
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter First Number", type: "number", name: "FirstNumber" },
+    { message: "Enter Second Number", type: "number", name: "SecondNumber" },
+    {
+        message: "Select One Of The Operators To Perform The Action",
+        type: "list",
+        name: "Operater",
+        choices: ["Addition", "Substraction", "Multipilication", "Divison"],
+    },
+]);
+// conditional operater
+if (answer.Operater === "Addition") {
+    console.log(answer.FirstNumber + answer.SecondNumber);
+}
+else if (answer.Operater === "Substraction") {
+    console.log(answer.FirstNumber - answer.SecondNumber);
+}
+else if (answer.Operater === "Multipilication") {
+    console.log(answer.FirstNumber * answer.SecondNumber);
+}
+else if (answer.Operater === "Divison") {
+    console.log(answer.FirstNumber / answer.SecondNumber);
+}
+else {
+    console.log("Enter valid number");
+}
---
+++ index.ts
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import Choices from "inquirer/lib/objects/choices.js";
+
+const answer = await inquirer.prompt([
+  { message: "Enter First Number", type: "number", name: "FirstNumber" },
+  { message: "Enter Second Number", type: "number", name: "SecondNumber" },
+  {
+    message: "Select One Of The Operators To Perform The Action",
+    type: "list",
+    name: "Operater",
+    choices: ["Addition", "Substraction", "Multipilication", "Divison"],
+  },
+]);
+// conditional operater
+if (answer.Operater === "Addition") {
+  console.log(answer.FirstNumber + answer.SecondNumber);
+} else if (answer.Operater === "Substraction") {
+  console.log(answer.FirstNumber - answer.SecondNumber);
+} else if (answer.Operater === "Multipilication") {
+  console.log(answer.FirstNumber * answer.SecondNumber);
+} else if (answer.Operater === "Divison") {
+  console.log(answer.FirstNumber / answer.SecondNumber);
+} else {
+  console.log("Enter valid number");
+}
